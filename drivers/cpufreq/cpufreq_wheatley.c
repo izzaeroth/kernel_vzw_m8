d8823b44755a26f8bce35ba1aebd27fd72d33b7b
izzaeroth <izzaeroth@gmail.com>
2015-01-03 14:55:12 -0600

    Update cpufreq_wheatley.c

--- drivers/cpufreq/cpufreq_wheatley.c
+++ drivers/cpufreq/cpufreq_wheatley.c
@@ -33,7 +33,7 @@
 #define MAX_SAMPLING_DOWN_FACTOR	(100000)
 #define MICRO_FREQUENCY_DOWN_DIFFERENTIAL	(3)
 #define MICRO_FREQUENCY_UP_THRESHOLD	(95)
-#define MICRO_FREQUENCY_MIN_SAMPLE_RATE	(10000)
+#define MICRO_FREQUENCY_MIN_SAMPLE_RATE	(50000)
 #define MIN_FREQUENCY_UP_THRESHOLD	(11)
 #define MAX_FREQUENCY_UP_THRESHOLD	(100)
 #define DEF_TARGET_RESIDENCY	(10000)
@@ -51,7 +51,7 @@
 #define MIN_SAMPLING_RATE_RATIO	(2)
 static unsigned int min_sampling_rate, num_misses;
 #define LATENCY_MULTIPLIER	(1000)
-#define MIN_LATENCY_MULTIPLIER	(100)
+#define MIN_LATENCY_MULTIPLIER	(20)
 #define TRANSITION_LATENCY_LIMIT	(10 * 1000 * 1000)
 static void do_dbs_timer(struct work_struct *work);
 static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
@@ -116,29 +116,31 @@ unsigned int allowed_misses;
 .target_residency = DEF_TARGET_RESIDENCY,
 .allowed_misses = DEF_ALLOWED_MISSES,
 };
-static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
-cputime64_t *wall)
+static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu,
+u64 *wall)
 {
-cputime64_t idle_time;
-cputime64_t cur_wall_time;
-cputime64_t busy_time;
+u64 idle_time;
+u64 cur_wall_time;
+u64 busy_time;
 cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
-busy_time = cputime64_add(kstat_cpu(cpu).cpustat.user,
-kstat_cpu(cpu).cpustat.system);
-busy_time = cputime64_add(busy_time, kstat_cpu(cpu).cpustat.irq);
-busy_time = cputime64_add(busy_time, kstat_cpu(cpu).cpustat.softirq);
-busy_time = cputime64_add(busy_time, kstat_cpu(cpu).cpustat.steal);
-busy_time = cputime64_add(busy_time, kstat_cpu(cpu).cpustat.nice);
-idle_time = cputime64_sub(cur_wall_time, busy_time);
+busy_time = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
+busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
+busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
+busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
+busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
+busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
+idle_time = cur_wall_time - busy_time;
 if (wall)
-*wall = (cputime64_t)jiffies_to_usecs(cur_wall_time);
-return (cputime64_t)jiffies_to_usecs(idle_time);
+*wall = jiffies_to_usecs(cur_wall_time);
+return jiffies_to_usecs(idle_time);
 }
 static inline cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall)
 {
-u64 idle_time = get_cpu_idle_time_us(cpu, wall);
+u64 idle_time = get_cpu_idle_time_us(cpu, NULL);
 if (idle_time == -1ULL)
 return get_cpu_idle_time_jiffy(cpu, wall);
+else
+idle_time += get_cpu_iowait_time_us(cpu, wall);
 return idle_time;
 }
 static inline cputime64_t get_cpu_iowait_time(unsigned int cpu, cputime64_t *wall)
@@ -307,7 +309,7 @@ dbs_info = &per_cpu(od_cpu_dbs_info, j);
 dbs_info->prev_cpu_idle = get_cpu_idle_time(j,
 &dbs_info->prev_cpu_wall);
 if (dbs_tuners_ins.ignore_nice)
-dbs_info->prev_cpu_nice = kstat_cpu(j).cpustat.nice;
+dbs_info->prev_cpu_nice = kcpustat_cpu(j).cpustat[CPUTIME_NICE];
 }
 return count;
 }
@@ -411,32 +413,29 @@ unsigned int idle_time, wall_time, iowait_time;
 unsigned int load, load_freq;
 int freq_avg;
 struct cpuidle_device * j_cpuidle_dev = NULL;
-struct cpuidle_state * deepidle_state = NULL;
-unsigned long long deepidle_time, deepidle_usage;
+// struct cpuidle_state * deepidle_state = NULL;
+// unsigned long long deepidle_time, deepidle_usage;
 j_dbs_info = &per_cpu(od_cpu_dbs_info, j);
 cur_idle_time = get_cpu_idle_time(j, &cur_wall_time);
 cur_iowait_time = get_cpu_iowait_time(j, &cur_wall_time);
-wall_time = (unsigned int) cputime64_sub(cur_wall_time,
-j_dbs_info->prev_cpu_wall);
+wall_time = (unsigned int) (cur_wall_time - j_dbs_info->prev_cpu_wall);
 j_dbs_info->prev_cpu_wall = cur_wall_time;
-idle_time = (unsigned int) cputime64_sub(cur_idle_time,
-j_dbs_info->prev_cpu_idle);
+idle_time = (unsigned int) (cur_idle_time - j_dbs_info->prev_cpu_idle);
 j_dbs_info->prev_cpu_idle = cur_idle_time;
-iowait_time = (unsigned int) cputime64_sub(cur_iowait_time,
-j_dbs_info->prev_cpu_iowait);
+iowait_time = (unsigned int) (cur_iowait_time - j_dbs_info->prev_cpu_iowait);
 j_dbs_info->prev_cpu_iowait = cur_iowait_time;
 if (dbs_tuners_ins.ignore_nice) {
 cputime64_t cur_nice;
 unsigned long cur_nice_jiffies;
-cur_nice = cputime64_sub(kstat_cpu(j).cpustat.nice,
-j_dbs_info->prev_cpu_nice);
+cur_nice = kcpustat_cpu(j).cpustat[CPUTIME_NICE] -
+j_dbs_info->prev_cpu_nice;
 /*
 * Assumption: nice time between sampling periods will
 * be less than 2^32 jiffies for 32 bit sys
 */
 cur_nice_jiffies = (unsigned long)
 cputime64_to_jiffies64(cur_nice);
-j_dbs_info->prev_cpu_nice = kstat_cpu(j).cpustat.nice;
+j_dbs_info->prev_cpu_nice = kcpustat_cpu(j).cpustat[CPUTIME_NICE];
 idle_time += jiffies_to_usecs(cur_nice_jiffies);
 }
 /*
@@ -457,6 +456,7 @@ load_freq = load * freq_avg;
 if (load_freq > max_load_freq)
 max_load_freq = load_freq;
 j_cpuidle_dev = per_cpu(cpuidle_devices, j);
+/*
 if (j_cpuidle_dev)
 deepidle_state = &j_cpuidle_dev->states[j_cpuidle_dev->state_count - 1];
 if (deepidle_state) {
@@ -467,6 +467,7 @@ total_usage += (unsigned long)(deepidle_usage - j_dbs_info->prev_idleusage);
 j_dbs_info->prev_idletime = deepidle_time;
 j_dbs_info->prev_idleusage = deepidle_usage;
 }
+*/
 }
 if (total_usage > 0 && total_idletime / total_usage >= dbs_tuners_ins.target_residency) {
 if (num_misses > 0)
@@ -608,7 +609,7 @@ j_dbs_info->prev_cpu_idle = get_cpu_idle_time(j,
 &j_dbs_info->prev_cpu_wall);
 if (dbs_tuners_ins.ignore_nice) {
 j_dbs_info->prev_cpu_nice =
-kstat_cpu(j).cpustat.nice;
+kcpustat_cpu(j).cpustat[CPUTIME_NICE];
 }
 }
 this_dbs_info->cpu = cpu;
@@ -668,10 +669,9 @@ return 0;
 }
 static int __init cpufreq_gov_dbs_init(void)
 {
-cputime64_t wall;
 u64 idle_time;
 int cpu = get_cpu();
-idle_time = get_cpu_idle_time_us(cpu, &wall);
+idle_time = get_cpu_idle_time_us(cpu, NULL);
 put_cpu();
 if (idle_time != -1ULL) {
 /* Idle micro accounting is supported. Use finer thresholds */
